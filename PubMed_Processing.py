bc4ae32aba061a32970943027a1aff8d0254c854
JJJAYYYZhao <71380690+JJJAYYYZhao@users.noreply.github.com>
2021-10-13 14:37:26 +0800

    Update PubMed_Processing.py

--- PubMed_Processing.py
+++ PubMed_Processing.py
@@ -1,7 +1,5 @@
-
 import pickle as pkl
 def load_cora():
-    names = ['x']
     with open("F:/焦糖/PubMed/ind.pubmed.ty", 'rb') as f:
             data = pkl.load(f, encoding='latin1')
             #print(data.shape)   #(18717, 500)-ind.pubmed.allx是18717行，500列,有500个特征
@@ -9,10 +7,9 @@ def load_cora():
             #n=data.todense()
             m=data.tolist()
             
-  # 变量data是个scipy.sparse.csr.csr_matrix，类似稀疏矩阵，输出得到的是矩阵中非0的行列坐标及值
+# 变量data是个scipy.sparse.csr.csr_matrix，类似稀疏矩阵，输出得到的是矩阵中非0的行列坐标及值
             
-            #nonzero=data.nonzero()
-            #print(nonzero[1]) 
+            #nonzero=data.nonzero()  非零
             #m=nonzero[1].tolist()
             #print(m)
             #输出非零元素对应的行坐标和列坐标
@@ -23,14 +20,4 @@ def load_cora():
                 for i in range(len(m)):
                    out_file.write(str(m[i])+"\n")
                    
-            
-            #print(data.toarray())
-# [[0. 0. 0. ... 0. 0. 0.]
-#  [0. 0. 0. ... 0. 0. 0.]
-#  [0. 0. 0. ... 0. 0. 0.]
-#  ...
-#  [0. 0. 0. ... 0. 1. 0.]
-#  [0. 0. 0. ... 0. 0. 0.]
-#  [0. 1. 0. ... 0. 0. 0.]]
-
 load_cora()
